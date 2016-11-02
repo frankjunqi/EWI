eaed8419e8c957adf93f6d4d6915731a936ba484
kjh08490 <kjh08490@ly.com>
2016-11-02 15:36:13 +0800

    app key sp

--- app/src/main/java/baranek/vojtech/ftpclient/api/Host.java
+++ app/src/main/java/baranek/vojtech/ftpclient/api/Host.java
@@ -5,31 +5,31 @@ package baranek.vojtech.ftpclient.api;
  */
 public class Host {
 
-    //public static String UpdateHost = "http://192.168.163.11:8070/";
-    public static String UpdateHost = "http://update.seekersoft.cn/";
+    public static String UpdateHost = "http://192.168.163.11:8070/";
+    //public static String UpdateHost = "http://update.seekersoft.cn/";
 
-    //public static String HOST = "http://192.168.163.11:8077/";
-    public static String HOST = "http://mes.seekersoft.cn/";
+    public static String HOST = "http://192.168.163.11:8077/";
+    //public static String HOST = "http://mes.seekersoft.cn/";
 
-    //public static String FTPHOST = "192.168.163.11";
-    public static String FTPHOST = "27.54.248.35";
+    public static String FTPHOST = "192.168.163.11";
+    //public static String FTPHOST = "27.54.248.35";
 
-    //public static int FTPHOSTIPTABLES = 8078;
-    public static int FTPHOSTIPTABLES = 6888;
+    public static int FTPHOSTIPTABLES = 8078;
+    //public static int FTPHOSTIPTABLES = 6888;
 
-    public static final String KWYHOST = "keyhost";
-    public static final String KWYFTPHOST = "keyftphost";
-    public static final String KWYFTPHOSTIPTABLES = "keyftphostiptables";
+    public static final String KWYHOST = "keyhost_version_101";
+    public static final String KWYFTPHOST = "keyftphost_version_101";
+    public static final String KWYFTPHOSTIPTABLES = "keyftphostiptables_version_101";
 
-    public static final String UPDATEKEYHOST = "updatehost";
+    public static final String UPDATEKEYHOST = "updatehost_version_101";
 
     // 时隔10秒请求一次
     public static int TENLOOPER = 10;
 
     public static int BOTTOMHEIGHT = 0;
 
-    public static  int TEMPLOOPER = 60*60;
+    public static int TEMPLOOPER = 60 * 60;
 
-    public static  int UPDATELOOPER = 60*60*6;
+    public static int UPDATELOOPER = 60 * 60 * 6;
 
 }
